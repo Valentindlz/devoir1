72fe4c84216b5c4a7869f1160111138f8a24c914
Valentin <delvallez.valentin@outlook.fr>
2022-10-05 11:06:43 +0200

    createbdd

---
+++ src/Entity/SecteurActivite.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\SecteurActiviteRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: SecteurActiviteRepository::class)]
+class SecteurActivite
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $intitule = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $Intitule = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getIntitule(): ?string
+    {
+        return $this->intitule;
+    }
+
+    public function setIntitule(string $intitule): self
+    {
+        $this->intitule = $intitule;
+
+        return $this;
+    }
+}
